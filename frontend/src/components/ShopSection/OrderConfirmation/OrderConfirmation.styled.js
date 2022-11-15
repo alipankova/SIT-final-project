34b4f3ee65bafe091feb6aebbc09baec4136841b
Stef <stef.guler@gmail.com>
2022-11-15 20:10:00 +0100

    some styling changes

--- frontend/src/components/ShopSection/OrderConfirmation/OrderConfirmation.styled.js
+++ frontend/src/components/ShopSection/OrderConfirmation/OrderConfirmation.styled.js
@@ -5,12 +5,13 @@ export const OrderConfirmationContainer = styled.div`
   display: flex;
   flex-direction: column;
   min-width: 100%;
+  min-height: 100vh;
   background:  #FAF5E1;
   align-items: center;
   font-size: 32px;
   margin: 0 auto;
   justify-content: center;
-  margin-top: 7.5rem;
+  padding-top: 6rem;
 `
 
 export const Content = styled.div`
